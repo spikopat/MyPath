1daad93f5b17cddc037f5618d397e32875bc82ae
Serdar Doğan <serdar.dogaan@gmail.com>
2022-08-16 03:29:16 +0300

    Last commit

--- MyPath/Assets/Scripts/ChibiController.cs
+++ MyPath/Assets/Scripts/ChibiController.cs
@@ -2,7 +2,6 @@ using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class ChibiController : MonoBehaviour
